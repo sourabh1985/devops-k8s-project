dfa42ff9dedc82f6423087da5c15c6faa8bc6c3b
Sourabh Jha <sourabhjha@Sourabhs-MacBook-Pro.local>
2026-05-28 16:26:49 +0530

    force rebuild backend image

--- backend/service1/app.py
+++ backend/service1/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/api')
 def home():
-    return {"message": "Service running"}
+    return {"message": "API working now."}  # small change
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
