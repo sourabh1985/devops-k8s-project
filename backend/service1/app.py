1f4bfa14147d48138b38a4777f9f44b200e0c4b7
Sourabh Jha <inbox.saurabh15@gmail.com>
2026-06-02 11:47:09 +0530

    Update app.py

--- backend/service1/app.py
+++ backend/service1/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/api')
 def home():
-    return {"message": "API working now."}  # small change
+    return {"message": "API working with gitops now."}  # small change
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
